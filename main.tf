9bf827a9b55bb14f137f02487fe8dbabee88701c
Srinivas <67258672+SriniSunviva@users.noreply.github.com>
2026-02-21 15:01:10 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,9 +1,42 @@
-# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
-    
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myAKSCluster"
+  location            = East US
+  resource_group_name = myResourceGroup
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "standard_d4ds_v5"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  linux_profile {
+    admin_username = "azureuser"
+
+    ssh_key {
+      key_data = file("~/.ssh/id_rsa.pub")
+    }
   }
 }
---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+  subscription_id = ""
+}
+# while register apps, we can get cclient id and Secret.

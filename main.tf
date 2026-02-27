5efd82026b44553d9a1ef82677f00492e75b79d8
Srinivas <67258672+SriniSunviva@users.noreply.github.com>
2026-02-27 11:12:07 -0800

    added

--- main.tf
+++ main.tf
@@ -10,33 +10,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
-  location = "East US"
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "myAKSCluster"
-  location            = East US
-  resource_group_name = myResourceGroup
-  dns_prefix          = "myaksdns"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "standard_d4ds_v5"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  linux_profile {
-    admin_username = "azureuser"
-
-    ssh_key {
-      key_data = file("~/.ssh/id_rsa.pub")
-    }
-  }
-}

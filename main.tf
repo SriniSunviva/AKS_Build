02efa0c335176847a6d85171bee4d910e24ca27f
Srinivas <67258672+SriniSunviva@users.noreply.github.com>
2026-02-27 11:20:40 -0800

    added

--- main.tf
+++ main.tf
@@ -1,15 +1,10 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+        source = "hashicorp/azurerm"
+        version = "~> 4.8.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = "myResourceGroup"

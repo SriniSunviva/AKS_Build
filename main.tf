77b3b256cb36603436bbcf5f7d320450239a5f70
Srinivas <67258672+SriniSunviva@users.noreply.github.com>
2026-02-27 12:08:22 -0800

    ac

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
+        source = "registry.terraform.io/hashicorp/azurerm"
         version = "~> 4.8.0"
     }
   }

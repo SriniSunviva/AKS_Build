7568cf91d7ee1ba76652b1071b305f57bfe83262
Srinivas <67258672+SriniSunviva@users.noreply.github.com>
2026-02-28 02:13:28 -0800

    updated

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "azurerm" {
       features {}
   client_id       = "1eaec185-6d1f-49ee-aac8-4cd752674738"
   client_secret   = "nEN8Q~gYKI5UODIk95kU~e46GtesaOAi3SWSNae."
-  tenant_id       = "25e5fd9f-f77b-4db6-b7cd-6a8d12732039"
-  subscription_id = "1bef7fc5-c411-4aa1-ab0d-8ebf5e61ff65"
+  tenant_id       = "1bef7fc5-c411-4aa1-ab0d-8ebf5e61ff65"
+  subscription_id = "25e5fd9f-f77b-4db6-b7cd-6a8d12732039"
 }

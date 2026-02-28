c36b5a2224c1639adc742971bb00c0069eaf7ea7
Srinivas <67258672+SriniSunviva@users.noreply.github.com>
2026-02-27 19:35:49 -0800

    added

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
       features {}
-  client_id       = "6d73f9f2-0035-4d03-88ee-02dd439585e8"
-  client_secret   = "fNB8Q~ts45Ie3~CmlnZh6u.910qf_RYvfkThjcDo"
-  tenant_id       = "1bef7fc5-c411-4aa1-ab0d-8ebf5e61ff65"
-  subscription_id = "25e5fd9f-f77b-4db6-b7cd-6a8d12732039"
+  client_id       = "1eaec185-6d1f-49ee-aac8-4cd752674738"
+  client_secret   = "nEN8Q~gYKI5UODIk95kU~e46GtesaOAi3SWSNae."
+  tenant_id       = "25e5fd9f-f77b-4db6-b7cd-6a8d12732039"
+  subscription_id = "1bef7fc5-c411-4aa1-ab0d-8ebf5e61ff65"
 }
